009b08f3918d358d5d07f1c3ab11ccdd08601c87
Jan Henning Thorsen <jhthorsen@cpan.org>
2014-02-21 18:23:16 +0100

    Released version 0.04
           * Fix PASS need to be sent before NICK and USER
             http://www.irchelp.org/irchelp/rfc/chapter4.html#c4_1_1
             Contributor: Stephan Jauernick

--- lib/Mojo/IRC.pm
+++ lib/Mojo/IRC.pm
@@ -6,7 +6,7 @@ Mojo::IRC - IRC Client for the Mojo IOLoop
 
 =head1 VERSION
 
-0.0303
+0.04
 
 =head1 SYNOPSIS
 
@@ -254,7 +254,7 @@ use constant DEBUG => $ENV{MOJO_IRC_DEBUG} ? 1 : 0;
 use constant DEFAULT_CERT => $ENV{MOJO_IRC_CERT_FILE} || catfile dirname(__FILE__), 'mojo-irc-client.crt';
 use constant DEFAULT_KEY => $ENV{MOJO_IRC_KEY_FILE} || catfile dirname(__FILE__), 'mojo-irc-client.key';
 
-our $VERSION = '0.0303';
+our $VERSION = '0.04';
 
 my @DEFAULT_EVENTS = qw( irc_ping irc_nick irc_notice irc_rpl_welcome irc_err_nicknameinuse );
 
