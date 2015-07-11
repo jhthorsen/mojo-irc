e10cde90bafeea501eee461688659564ebc05675
Jan Henning Thorsen <jhthorsen@cpan.org>
2015-07-11 13:26:29 +0200

    Released version 0.26
     - Bumping Mojolicious requirement to 6.00

--- lib/Mojo/IRC.pm
+++ lib/Mojo/IRC.pm
@@ -6,7 +6,7 @@ Mojo::IRC - IRC Client for the Mojo IOLoop
 
 =head1 VERSION
 
-0.25
+0.26
 
 =head1 SYNOPSIS
 
@@ -117,7 +117,7 @@ use constant DEFAULT_CERT => $ENV{MOJO_IRC_CERT_FILE} || catfile dirname(__FILE_
 use constant DEFAULT_KEY  => $ENV{MOJO_IRC_KEY_FILE}  || catfile dirname(__FILE__), 'mojo-irc-client.key';
 use constant OFFLINE => $ENV{MOJO_IRC_OFFLINE} ? 1 : 0;
 
-our $VERSION = '0.25';
+our $VERSION = '0.26';
 
 my %CTCP_QUOTE = ("\012" => 'n', "\015" => 'r', "\0" => '0', "\cP" => "\cP");
 
